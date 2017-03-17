d0d1e3cf5fbd6863dfc2aeecc03194f859109b1e
BK Mac <ahadcove@gmail.com>
2017-03-17 08:59:34 -0400

    Css touchups

---
+++ .history/src/app/shared/menu/menu.component_20170317081455.ts
@@ -0,0 +1,34 @@
+import { Component, Input, Output, EventEmitter, trigger, state, style, transition, animate, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss'],
+    animations: [
+    trigger(
+      'enterAnimation', [
+        transition(':enter', [
+          style({transform: 'translateY(-100%)', opacity: 0, "z-index":-99}),
+          animate('500ms', style({transform: 'translateY(0)', opacity: 1, "z-index":99}))
+        ]),
+        transition(':leave', [
+          style({transform: 'translateY(0)', opacity: 1, "z-index":99}),
+          animate('500ms', style({transform: 'translateY(-100%)', opacity: 0, "z-index":-99}))
+        ])
+      ]
+    )
+  ]
+})
+export class MenuComponent implements OnInit {
+  public menuOpen:Boolean = false;
+
+  openMenu(){
+    this.menuOpen = !this.menuOpen;
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ .history/src/app/shared/menu/menu.component_20170317082215.ts
@@ -0,0 +1,34 @@
+import { Component, Input, Output, EventEmitter, trigger, state, style, transition, animate, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss'],
+    animations: [
+    trigger(
+      'enterAnimation', [
+        transition(':enter', [
+          style({transform: 'translateX(20%)', opacity: 0, "z-index":-99}),
+          animate('500ms', style({transform: 'translateX(0)', opacity: 1, "z-index":99}))
+        ]),
+        transition(':leave', [
+          style({transform: 'translateX(0)', opacity: 1, "z-index":99}),
+          animate('500ms', style({transform: 'translateX(20%)', opacity: 0, "z-index":-99}))
+        ])
+      ]
+    )
+  ]
+})
+export class MenuComponent implements OnInit {
+  public menuOpen:Boolean = false;
+
+  openMenu(){
+    this.menuOpen = !this.menuOpen;
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
